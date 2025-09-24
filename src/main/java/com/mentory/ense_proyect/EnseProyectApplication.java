04e8db8c3477062479e4d0dc835137bacf5dccc4
notlinkk <martintxogm@gmail.com>
2025-09-24 11:17:07 +0200

    Creación del proyecto

---
+++ src/main/java/com/mentory/ense_proyect/EnseProyectApplication.java
@@ -0,0 +1,13 @@
+package com.mentory.ense_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnseProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnseProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentory/ense_proyect/EnseProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentory.ense_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnseProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
